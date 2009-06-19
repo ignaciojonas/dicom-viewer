36648a870cc9470c9aaf06b6482e276ad2e5d926
ignaciojonas <ignaciojonas@f1afd222-4706-11de-a65c-33beeba39556>
2009-06-19 19:44:14 +0000

    Run Funciona sin que se vea la consola

--- trunk/dicom-viewer/run.bat
+++ trunk/dicom-viewer/run.bat
@@ -1,4 +1,3 @@
-echo on
-java  -classpath "src;lib/dcm4che.jar;lib/dcm4che-imageio-rle-2.0.12.jar;lib/jai_imageio.jar:lib/log4j.jar;lib/BaseCode.jar;lib/dcm4che-core-2.0.16.jar;lib/jogl.jar;lib/log4j.jar;lib/slf4j-api-1.4.3.jar;lib/slf4j-log4j12-1.4.3.jar" ui.MainFrame
-pause
-echo off
+START /B javaw.exe  -Xms256m -Xmx1024m -XX:-UseGCOverheadLimit -classpath "src;lib/dcm4che.jar;lib/dcm4che-imageio-rle-2.0.12.jar;lib/jai_imageio.jar:lib/log4j.jar;lib/BaseCode.jar;lib/dcm4che-core-2.0.16.jar;lib/jogl.jar;lib/log4j.jar;lib/slf4j-api-1.4.3.jar;lib/slf4j-log4j12-1.4.3.jar" ui.MainFrame
+
+
